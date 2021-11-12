4afb53de710901d869e6ebe81d8708e521527cb8
gt74745 <garrison.m.taylor@gmail.com>
2021-11-12 12:25:49 -0600

    Made rotation asynchronous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -20,7 +20,7 @@ public class Drive extends Core {
         joystick_y = gamepad1.left_stick_y;
         joystick_x = (gamepad1.left_stick_x == 0) ? 0.000001 :
                 gamepad1.left_stick_x;
-        joystick_pivot = gamepad1.right_stick_x;
+        joystick_pivot = 0.4 * (gamepad1.right_stick_x);
 
         // Find out the distance of the joystick from resting position to control speed
         joystick_power = Math.sqrt(Math.pow(joystick_x, 2) + Math.pow(joystick_y, 2));
@@ -33,16 +33,11 @@ public class Drive extends Core {
                 (Math.atan(-joystick_y / joystick_x) + Math.PI - Math.PI / 4) - gyro_angles.firstAngle;
 
         // Pass that angle through a pair of wave functions to get the power for each corresponding pair of parallel wheels
-        negative_power = 0.3 * (joystick_power * Math.sin(orientation));
-        positive_power = (orientation != 0) ? 0.3 * (joystick_power * Math.cos(orientation)) :
+        negative_power = 0.45 * (joystick_power * Math.sin(orientation));
+        positive_power = (orientation != 0) ? 0.45 * (joystick_power * Math.cos(orientation)) :
                 negative_power;
 
         // This is all we need to actually move the robot, method decs in Core.java
-        if (joystick_pivot != 0)
-        {
-            pivot(0.3 * joystick_pivot);
-        }
-        else
-            move(positive_power, negative_power);
+        move(positive_power, negative_power, joystick_pivot);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/localization/Encoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/localization/Encoder.java
@@ -39,13 +39,4 @@ public class Encoder {
         previousRightBackMotorPos = rightBackMotorPos;
 
     }
-
-        public void getEncoderPosition() {
-
-            HardwareUtil.motors.
-
-
-        }
-    }
-
 }
