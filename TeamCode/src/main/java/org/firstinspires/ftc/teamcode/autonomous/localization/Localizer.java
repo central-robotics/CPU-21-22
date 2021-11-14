d2cec87ce2f7bcfd32366e0fb7896cc89fdb0ac9
danielhuinda <huindam@gmail.com>
2021-11-14 12:34:03 -0600

    Added position to localizer. Robot will read its position off of a constantly updated position (x, y, t).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/localization/Localizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/localization/Localizer.java
@@ -12,6 +12,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import java.util.Vector;
 
 public class Localizer {
+    public Position robotPos;
+
     private Vision vision;
     private Encoder encoder;
 
@@ -19,11 +21,10 @@ public class Localizer {
     {
         vision = new Vision();
         vision.initializeLocalizer();
-
         vision.targets.activate();
     }
 
-    public Location getRobotPosition()
+    private Location getRobotPosition()
     {
         for (VuforiaTrackable trackable : vision.trackables) {
             if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
@@ -57,9 +58,22 @@ public class Localizer {
        return null;
     }
 
-    public class Location
+    public void updatePosition()
+    {
+        if (robotPos == null)
+            robotPos = new Position();
+
+
+    }
+
+    private class Location //Position relative to vision targets
     {
         VectorF translation;
         Orientation orientation;
+    }
+
+    private class Position //Position of robot on field (x, y, theta)
+    {
+        Float x, y, t;
     }
 }
