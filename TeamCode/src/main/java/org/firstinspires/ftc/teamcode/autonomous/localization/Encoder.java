10792707b5ac23d3aa419f058142235ab074eb61
Zack Murry <zackk.murryy@gmail.com>
2021-11-18 15:53:44 -0600

    Fix encoder algorithm and gitignore .idea

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/localization/Encoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/localization/Encoder.java
@@ -69,8 +69,8 @@ public class Encoder {
         double deltaX, deltaY;
 
         //Compute displacement in robot reference frame.
-        deltaX = (lfDisp + robotRfDisp - robotRbDisp - robotLbDisp) / (2 * Math.sqrt(2));
-        deltaY = (lfDisp - robotRfDisp - robotRbDisp + robotLbDisp) / (2 * Math.sqrt(2));
+        deltaX = (robotLfDisp + robotRfDisp - robotRbDisp - robotLbDisp) / (2 * Math.sqrt(2));
+        deltaY = (robotLfDisp - robotRfDisp - robotRbDisp + robotLbDisp) / (2 * Math.sqrt(2));
 
         //Robot theta
         double theta;
