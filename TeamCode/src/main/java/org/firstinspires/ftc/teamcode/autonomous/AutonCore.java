8fff6ebc244d39bd025ec5391ea51266f8bdbd4a
danielhuinda <huindam@gmail.com>
2021-11-18 19:09:56 -0600

    Added navigation to AutonCore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonCore.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.autonomous.hardware.Hardware;
 import org.firstinspires.ftc.teamcode.autonomous.localization.Localization;
 import org.firstinspires.ftc.teamcode.autonomous.localization.Position;
+import org.firstinspires.ftc.teamcode.autonomous.waypoint.Navigation;
 import org.firstinspires.ftc.teamcode.autonomous.waypoint.Waypoint;
 
 public class AutonCore extends LinearOpMode {
@@ -13,13 +14,14 @@ public class AutonCore extends LinearOpMode {
     public static ElapsedTime runtime;
     public Position robotPosition;
     private Localization localization;
+    private Navigation navigation;
     private int instruction;
 
     @Override
     public void runOpMode() {
         hardware = new Hardware(hardwareMap);
         localization = new Localization(hardware);
-
+        navigation = new Navigation(hardware, localization);
         robotPosition = localization.getRobotPosition();
 
         waitForStart();
