f4811b3aa96c23ff189b56759fa463ec18c6dced
danielhuinda <huindam@gmail.com>
2021-11-27 14:13:03 -0600

    added telemetry for debugging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonCore.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.autonomous.hardware.Hardware;
 import org.firstinspires.ftc.teamcode.autonomous.localization.Localization;
 import org.firstinspires.ftc.teamcode.autonomous.localization.Position;
 import org.firstinspires.ftc.teamcode.autonomous.localization.Velocity;
 import org.firstinspires.ftc.teamcode.autonomous.waypoint.Navigation;
 
+@Autonomous
 public class AutonCore extends LinearOpMode {
     public Hardware hardware;
     public static ElapsedTime runtime;
@@ -23,6 +26,7 @@ public class AutonCore extends LinearOpMode {
         hardware = new Hardware(hardwareMap);
         localization = new Localization(hardware);
         navigation = new Navigation(hardware, localization);
+        runtime = new ElapsedTime();
 
         waitForStart();
 
@@ -33,6 +37,14 @@ public class AutonCore extends LinearOpMode {
             robotPosition = localization.getRobotPosition();
             localization.increment(robotPosition);
             robotVelocity = localization.getRobotVelocity(runtime);
+            telemetry.addData("runtime", runtime);
+            telemetry.addData("X", robotPosition.x);
+            telemetry.addData("Y", robotPosition.y);
+            telemetry.addData("t", robotPosition.t);
+            telemetry.addData("dX", robotVelocity.dx);
+            telemetry.addData("dY", robotVelocity.dy);
+            telemetry.addData("dt", robotVelocity.dt);
+            telemetry.update();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Constants.java
@@ -3,5 +3,5 @@ package org.firstinspires.ftc.teamcode.autonomous;
 public final class Constants {
     //This class is home to all of the drive and field constants that are used multiple times in the code.
 
-    public static final double DISTANCE_PER_TICK = 0.56089; //5202 Series Motor (PPR)
+    public static final double DISTANCE_PER_TICK = 0.701248; //5202 Series Motor (PPR)
 }
